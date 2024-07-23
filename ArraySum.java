e5e246b8c18809f3fe031b3ca9120eec65d63465
Polishetty ananya <ananyapolishetty@gmail.com>
2024-07-23 21:49:15 +0530

    Create ArraySum.java

---
+++ ArraySum.java
@@ -0,0 +1,18 @@
+package code;
+
+public class ArraySum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[]= {1,2,3,4,5};
+		int sum=0;
+		int i;
+		for(i=0;i<a.length;i++)
+		{
+			sum =sum+a[i];
+		}
+		
+System.out.println("sum:"+sum);
+	}
+
+}
