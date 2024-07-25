95752132b4c6a8e1b5b556a461ddff1f6742210d
Polishetty ananya <ananyapolishetty@gmail.com>
2024-07-25 09:33:34 +0530

    Create  HardCode.java

---
+++ HardCode.java
@@ -0,0 +1,26 @@
+package code;
+
+public class HardCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int size=3;
+switch (size)
+{
+case 1:
+	System.out.println("Size is small");
+break;
+case 2:
+	System.out.println("Size is medium");
+break;
+case 3:
+	System.out.println("Size is large");
+break;
+default:
+	System.out.println("Size is undecided");
+
+}
+
+	}
+
+}
